333e5853f4cdea27a42eb059286fbe0e062c35c7
Obed-creator1 <obedpianoman@gmail.com>
2026-02-12 17:50:24 +0200

    Fix: add Render domain to ALLOWED_HOSTS

--- backend/high_prosper/settings.py
+++ backend/high_prosper/settings.py
@@ -50,7 +50,7 @@ if not firebase_credentials:
 # SECURITY
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost,high-prosper-empire.onrender.com").split(",")
 
 
 # Applications
